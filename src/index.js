cc60baeb26546551e2fa64d987e136bb5e6a3466
Hryhory Lebedzev <hryhory.lebedzev@itechart-group.com>
2019-06-18 20:55:57 +0300

    was added helmet to provide security

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+const helmet = require('helmet');
 const cookieParser = require('cookie-parser');
 const userData = require('./middleware/user-data');
 
@@ -8,6 +9,7 @@ app
     .use(express.urlencoded({ extended: true }))
     .use(express.json())
     .use(cookieParser())
+    .use(helmet())
     .use(userData)
     .use(require('./routes/login'));
 
